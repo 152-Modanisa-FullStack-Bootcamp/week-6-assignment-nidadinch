d262a32ca0002376aed96d51918b3b7d2ad907df
Nida Dinç <20160808047@ogr.akdeniz.edu.tr>
2022-02-13 11:55:23 +0300

    delete unnecessary tests

--- data/data_test.go
+++ data/data_test.go
@@ -10,138 +10,116 @@ import (
 
 var conf = config.Get()
 
-func TestGetAllWallets_Empty(t *testing.T) {
-
-	tests := []struct {
-		name    string
-		d       *data.Data
-		want    []*data.Wallet
-		wantErr bool
-	}{
-		{"testCase01", &data.Data{}, []*data.Wallet{}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			got, err := d.GetAllWallets()
-			assert.Nil(t, err)
-			assert.Equal(t, got, tt.want)
-		})
-	}
-}
-
 func TestGetAllWallets(t *testing.T) {
+	t.Run("should return all wallets correctly", func(t *testing.T) {
+		tests := []struct {
+			name    string
+			d       *data.Data
+			want    []*data.Wallet
+			wantErr bool
+		}{
+			{"testCase01", &data.Data{}, []*data.Wallet{{Username: "Nida", Balance: 666}, {Username: "John", Balance: 999}}, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := data.NewData()
+				wallet1 := data.Wallet{Username: "Nida", Balance: 666}
+				wallet2 := data.Wallet{Username: "John", Balance: 999}
+				d.NewWallet(&wallet1)
+				d.NewWallet(&wallet2)
 
-	tests := []struct {
-		name    string
-		d       *data.Data
-		want    []*data.Wallet
-		wantErr bool
-	}{
-		{"testCase01", &data.Data{}, []*data.Wallet{{Username: "Nida", Balance: 666}, {Username: "John", Balance: 999}}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			wallet1 := data.Wallet{Username: "Nida", Balance: 666}
-			wallet2 := data.Wallet{Username: "John", Balance: 999}
-			d.NewWallet(&wallet1)
-			d.NewWallet(&wallet2)
+				got, err := d.GetAllWallets()
+				assert.Nil(t, err)
+				assert.Equal(t, got, tt.want)
+			})
+		}
+	})
 
-			got, err := d.GetAllWallets()
-			assert.Nil(t, err)
-			assert.Equal(t, got, tt.want)
-		})
-	}
+	t.Run("should return empty wallet if inMemoryDB is empty", func(t *testing.T) {
+		tests := []struct {
+			name    string
+			d       *data.Data
+			want    []*data.Wallet
+			wantErr bool
+		}{
+			{"testCase01", &data.Data{}, []*data.Wallet{}, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := data.NewData()
+				got, err := d.GetAllWallets()
+				assert.Nil(t, err)
+				assert.Equal(t, got, tt.want)
+			})
+		}
+	})
 }
 
 func TestGetWallet(t *testing.T) {
+	t.Run("should return wallet by username", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			d        *data.Data
+			username string
+			want     *data.Wallet
+			wantErr  bool
+		}{
+			{"testCase01", &data.Data{}, "Nida", &data.Wallet{Username: "Nida", Balance: 666}, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := data.NewData()
+				wallet1 := data.Wallet{Username: "Nida", Balance: 666}
+				d.NewWallet(&wallet1)
+				got, err := d.GetWallet(tt.username)
+				assert.Nil(t, err)
+				assert.Equal(t, got, tt.want)
+			})
+		}
+	})
 
-	tests := []struct {
-		name     string
-		d        *data.Data
-		username string
-		want     *data.Wallet
-		wantErr  bool
-	}{
-		{"testCase01", &data.Data{}, "Nida", &data.Wallet{Username: "Nida", Balance: 666}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			wallet1 := data.Wallet{Username: "Nida", Balance: 666}
-			d.NewWallet(&wallet1)
-			got, err := d.GetWallet(tt.username)
-			assert.Nil(t, err)
-			assert.Equal(t, got, tt.want)
-		})
-	}
+	t.Run("should return empty wallet if wallet doesn't exist", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			d        *data.Data
+			username string
+			want     *data.Wallet
+			wantErr  bool
+		}{
+			{"testCase01", &data.Data{}, "Nida", &data.Wallet{}, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := data.NewData()
+				got, err := d.GetWallet(tt.username)
+				assert.Error(t, err, "user with ID '%v' not found", tt.username)
+				assert.Equal(t, got, tt.want)
+			})
+		}
+	})
+
 }
-func TestGetWallet_Empty(t *testing.T) {
-
-	tests := []struct {
-		name     string
-		d        *data.Data
-		username string
-		want     *data.Wallet
-		wantErr  bool
-	}{
-		{"testCase01", &data.Data{}, "Nida", &data.Wallet{}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			got, err := d.GetWallet(tt.username)
-			assert.Error(t, err, "user with ID '%v' not found", tt.username)
-			assert.Equal(t, got, tt.want)
-		})
-	}
-}
-
 func TestData_AddWallet(t *testing.T) {
-
-	tests := []struct {
-		name                 string
-		d                    *data.Data
-		username             string
-		initialBalanceAmount int
-		want                 *data.Wallet
-		wantErr              bool
-	}{
-		{"testCase01", &data.Data{}, "Nida", conf.InitialBalanceAmount, &data.Wallet{Username: "Nida", Balance: conf.InitialBalanceAmount}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			got, err := d.AddWallet(tt.username, tt.initialBalanceAmount)
-			assert.Nil(t, err)
-			assert.Equal(t, got, tt.want)
-		})
-	}
-}
-
-func TestAddWallet_Existing(t *testing.T) {
-
-	tests := []struct {
-		name                 string
-		d                    *data.Data
-		username             string
-		initialBalanceAmount int
-		want                 *data.Wallet
-		wantErr              bool
-	}{
-		{"testCase01", &data.Data{}, "Nida", conf.InitialBalanceAmount, &data.Wallet{Username: "Nida", Balance: conf.InitialBalanceAmount}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := data.NewData()
-			wallet1 := data.Wallet{Username: "Nida", Balance: conf.InitialBalanceAmount}
-			d.NewWallet(&wallet1)
-			got, err := d.AddWallet(tt.username, tt.initialBalanceAmount)
-			assert.Nil(t, err)
-			assert.Equal(t, got, tt.want)
-		})
-	}
+	t.Run("should create a new wallet", func(t *testing.T) {
+		tests := []struct {
+			name                 string
+			d                    *data.Data
+			username             string
+			initialBalanceAmount int
+			want                 *data.Wallet
+			wantErr              bool
+		}{
+			{"testCase01", &data.Data{}, "Nida", conf.InitialBalanceAmount, &data.Wallet{Username: "Nida", Balance: conf.InitialBalanceAmount}, false},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				d := data.NewData()
+				got, err := d.AddWallet(tt.username, tt.initialBalanceAmount)
+				assert.Nil(t, err)
+				assert.Equal(t, got, tt.want)
+			})
+		}
+	})
 }
 
 func TestUpdateWallet(t *testing.T) {
--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module week-6-assingment-nidadinch
 
 go 1.17
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.7.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/tools v0.1.9 // indirect
